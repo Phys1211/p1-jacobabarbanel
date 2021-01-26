64c0fb927327e97d36ba2ea60b9c24ca724a13b5
jacobabarbanel <77994374+jacobabarbanel@users.noreply.github.com>
2021-01-26 12:41:27 -0500

    Update prob1.m

--- prob1.m
+++ prob1.m
@@ -1,3 +1,9 @@
-x = input("Input a number");
-disp("You inputted" )
-x
+
+GConstant=6.67*10^-11;
+REarth = 6.371*10^6;
+MEarth = 5.972*10^24;
+
+
+%x = input("Input a number");
+%disp("You inputted" )
+%x
